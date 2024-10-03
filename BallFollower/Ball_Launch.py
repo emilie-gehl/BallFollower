1f7eabc067608fbac530fef2628d3b50fcc558e8
Theo <you@example.com>
2024-10-03 08:55:28 +0200

    deplacement custom_interfaces

---
+++ BallFollower/Ball_Launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+   return LaunchDescription([
+       # Noeud BallFollower
+       Node(
+           package='BallFollower',
+           executable='Ball_Publisher',
+           name='Ball_Publisher',
+           output='screen'
+       ),
+       Node(
+           package='BallFollower',
+           executable='BallFollower',
+           name='BallFollower',
+           output='screen'
+       ),
+       # Inclusion du fichier robot.launch.py
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource('/turtlebot3/turtlebot3_bringup/launch/robot.launch.py'),
+            # Vous pouvez passer des arguments ici si nécessaire
+        )
+   ])
