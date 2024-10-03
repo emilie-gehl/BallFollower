907240ff89d425753b2abf825c4da440f26d3020
Emilie <emilie.gehl@yahoo.fr>
2024-10-03 12:36:34 +0200

    correction fichier launch

--- launch/Ball_Launch.py
+++ launch/Ball_Launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
